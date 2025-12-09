5af39662a34922f344f20b0ec184854dc1afbd0f
Ronald G Minnich <rminnich@gmail.com>
2025-12-09 10:58:12 -0800

    vmthread: Cleanup for
    Signed-off-by: Ronald G Minnich <rminnich@gmail.com>

--- tools/testing/vmthreads/vmthread.c
+++ tools/testing/vmthreads/vmthread.c
@@ -33,43 +33,81 @@ unsigned char stack[4096];
 int main(int argc, char *argv[])
 {
 	struct clone_args args;
+	char *buf = calloc(512, 1);
 	int pid = getpid();
 	int fpid = open("/sys/kernel/tracing/set_ftrace_pid", 2);
-	if (fpid < 0) {
-		errx(1, "can't open set_ftrace_pid");
-	}
+	int tracing_on = open("/sys/kernel/tracing/tracing_on", 2);
+	int trace = 0, vmthread = 0;
 
-	char *buf = calloc(512, 1);
-	buf[0] = '\n';
-	if (write(fpid, buf, 1) < 0){
-		errx(1, "can't write newline to fpid\n");
+	for(int i = 1; i < argc; i++) {
+		switch(argv[i][0]) {
+		case 't':
+			trace = 1;
+			break;
+		case 'v':
+			vmthread = 1;
+			break;
+		default: 
+			errx(1, "bad switch %s\n", argv[i]);
+			break;
+		}
 	}
-	sprintf(buf, "%d\n", pid);
-	if (write(fpid, buf, strlen(buf)) < 0){
-		errx(1, "can't write %s to fpid\n", buf);
+	if (trace > 1) {
+		if (fpid < 0) {
+			errx(1, "can't open set_ftrace_pid");
+		}
+
+		if (fpid < 0) {
+			errx(1, "can't open tracing_on");
+		}
+
+		buf[0] = '\n';
+		buf[1] = 0;
+		if (write(fpid, buf, strlen(buf)) < 0){
+			errx(1, "can't write newline to fpid\n");
+		}
+		sprintf(buf, "%d\n", pid);
+		if (write(fpid, buf, strlen(buf)) < 0){
+			errx(1, "can't write %s to fpid\n", buf);
+		}
 	}
 
 	memset(&args, 0, sizeof(args));
-	
+
 	/* Required: child exit signal */
 	args.exit_signal = SIGCHLD;
 	if (argc > 1)
 		args.flags |= CLONE_VMTHREAD;
-	
+
+	sprintf(buf, "1\n");
+	if (trace && write(tracing_on, buf, strlen(buf)) < 0){
+		errx(1, "can't write newline to fpid\n");
+	}
 	long ret = syscall(__NR_clone3,  &args, sizeof(args));
-	
+	sprintf(buf, "0\n");
+	if (trace && write(tracing_on, buf, 1) < 0){
+		perror("can't write 0\\n to tracing_on\n");
+	}
+
 	switch (ret) {
 	case 0:
 		/* Child */
 		printf("child: pid=%d ppid=%d\n", getpid(), getppid());
+		return 1;
 	case -1:
 		perror("clone3");
 	default:
 		wait(NULL);
 		/* Parent */
 		printf("parent: child pid=%ld\n", ret);
-		system("cat /sys/kernel/tracing/trace");
+		if (trace)
+			system("cat /sys/kernel/tracing/trace");
 	}
 
+	sprintf(buf, "0\n");
+	if (trace && write(tracing_on, buf, 1) < 0){
+		errx(1, "can't write 0\\n to tracing_on\n");
+	}
 	return 1;
 }
+
